65054d0c7e60cb69897e3cc5157d340ed2de5946
Patrick Hogenboom <patrick.hogenboom@pm.me>
2022-05-31 00:57:44 +0200

    Read potmeter and display values on VU meters
    Pot drives VU1 and amplitude of sine on VU2

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 #include <math.h>
 
 #define PotPin1 A0
+#define PotPin2 A1
 #define LedPin 13
-#define VUPin 5
+#define VUPin1 5
+#define VUPin2 6
 void setup() {
   Serial.begin(9600);
   pinMode(PotPin1, INPUT);
   pinMode(LedPin, OUTPUT);
-  pinMode(VUPin, OUTPUT);
+  pinMode(VUPin1, OUTPUT);
+  pinMode(VUPin2, OUTPUT);
 }
 
 float MapFloat(float x, float in_min, float in_max, float out_min, float out_max)
@@ -18,30 +21,29 @@ float MapFloat(float x, float in_min, float in_max, float out_min, float out_max
 
 float GenerateWave()
 {
-  float SinePeriod = 5;
+  float SinePeriod = 2;
   float wave = sin(millis() * (2*PI) / 1000 / SinePeriod);
 
   return wave;
 }
 void loop() {
+  digitalWrite(LedPin, 1);
+
   float voltageScale = 0.15/5.0; // VU input range / default pin voltage output range
 
-  analogWrite(5, MapFloat(GenerateWave(), -1, 1, 0, 255) * voltageScale);
+  // Display 2 sine waves on the VU meters
+  // analogWrite(VUPin1, MapFloat(GenerateWave(), -1, 1, 0, 255) * voltageScale);
+  // analogWrite(VUPin2, MapFloat(GenerateWave(), 1, -1, 0, 255) * voltageScale);
 
-  // int value = analogRead(PotPin);
-  // value = map(value, 0, 1023, 0, 255);
-  // analogWrite(5, value * voltageScale);
+  int value;
 
-  // for(int i=0; i<255; i++)
-  // {
-  //   analogWrite(5, i * voltageScale);
-  //   delay(20);
-  // }
-  // for(int i=255; i>0; i--)
-  // {
-  //   analogWrite(5, i * voltageScale);
-  //   delay(20);
-  // }
+  // Read potmeter2 and display on VU1
+  value = analogRead(PotPin2);
+  value = map(value, -100, 1023, 0, 255);
+  analogWrite(VUPin1, value * voltageScale);
+
+  // Display sine wave on VU2 with amplitude specified by potmeter2
+  analogWrite(VUPin2, MapFloat(GenerateWave() * value, 255, -255, 0, 255) * voltageScale);
 
   delay(10);
 }
