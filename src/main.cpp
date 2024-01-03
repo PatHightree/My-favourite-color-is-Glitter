29752e5b15e06c7b1eaab334d60705b69b2e3048
Patrick Hogenboom <patrick.hogenboom@pm.me>
2024-01-03 04:41:34 -0800

    Simple on/off motor control with L293D board and MotorDriver library
    https://www.hobbyelectronica.nl/product/l293d-motor-shield/
    https://github.com/CuriosityGym/MotorDriver

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,25 @@
 #include <arduino.h>
 #include <math.h>
-#include <Servo.h>
+#include <MotorDriver.h>
 #include <ultrasonic.h>
-
-#define ServoPin 9
 
 // Configuration
 bool DebugLog = true;
 float SinePeriod = 5;
-int MaxServoOutput = 180;
 int MinSonarDistance = 5;
+int MotorID = 3;
 
 // Variables
-Servo myservo;
-int angle = 0;
+MotorDriver m;
 
 void setup() {
   Serial.begin(9600);
   ultrasonic_setup();
-  myservo.attach(ServoPin);
 }
 
 float MapFloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void DebugLogGraph(float value)
-{
-    int graphWidth = 20;
-
-    Serial.print('|');
-    for(int i = 0; i < graphWidth; i++)
-      if (value * graphWidth / MaxServoOutput > i && 
-          value * graphWidth / MaxServoOutput < i+1)
-        Serial.print("#");
-      else
-        Serial.print(" " );
-      Serial.println('|');
 }
 
 float GenerateWave()
@@ -50,28 +32,34 @@ float GenerateWave()
 }
 
 void loop() {
-  // Sine wave is the basic output [-1,1]
-  float wave = GenerateWave();
+  // // Sine wave is the basic output [-1,1]
+  // float wave = GenerateWave();
   
-  // Obrain sonar distance [5,50]cm => sonarMultiplier [0,1]
-  int distance = max(ultrasonic_loop()-MinSonarDistance, 0);
-  float sonarMultiplier = constrain(distance, 0.0, 50.0) / 50.0;
+  // // Obrain sonar distance [5,50]cm => sonarMultiplier [0,1]
+  // int distance = max(ultrasonic_loop()-MinSonarDistance, 0);
+  // float sonarMultiplier = constrain(distance, 0.0, 50.0) / 50.0;
   
-  // Apply sonar multiplier
-  wave = wave * sonarMultiplier;
-  // Map to servo signal range
-  byte servoOutput = MapFloat(wave, -1, 1, 0, MaxServoOutput);
+  // // Apply sonar multiplier
+  // wave = wave * sonarMultiplier;
+  // // Map to servo signal range
+  // byte servoOutput = MapFloat(wave, -1, 1, 0, 255);
 
-  if (DebugLog) 
-  {
-    Serial.print("\t");
-    Serial.print(distance);
-    Serial.print("\t");
-    Serial.print(sonarMultiplier);
-    Serial.print("\t");
-    Serial.print(servoOutput);
-    Serial.println();
-  }  
+  // if (DebugLog) 
+  // {
+  //   Serial.print("\t");
+  //   Serial.print(distance);
+  //   Serial.print("\t");
+  //   Serial.print(sonarMultiplier);
+  //   Serial.print("\t");
+  //   Serial.print(servoOutput);
+  //   Serial.println();
+  // }  
 
-  myservo.write(servoOutput);
+  // m.motor(1,FORWARD,servoOutput);
+  Serial.print("On\n");
+  m.motor(MotorID, FORWARD, 255);
+  delay(1000);
+  Serial.print("Off\n");
+  m.motor(MotorID, FORWARD, 0);
+  delay(10000);
 }
--- src/pwm.h
+++
@@ -1,5 +0,0 @@
-#include<Arduino.h>
-
-void pwm_setup();
-void pwm_loop(byte duty);
-void setPwmDuty(byte duty);
