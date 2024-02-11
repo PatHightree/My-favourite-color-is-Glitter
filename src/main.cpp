e012ecc8905ab72b6f96573e41ab2f527ffbd6ba
Patrick Hogenboom <patrick.hogenboom@pm.me>
2024-02-12 00:02:09 +0100

    Code from monkeyfist forum post, logs but no movement
    https://forum.arduino.cc/t/using-a-tmc2209-silent-stepper-motor-driver-with-an-arduino/666992/14

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,73 @@
-// Using 
-// Arduino Nano
-// L298N motor shield, powered at 12V, driving a PSU fan motor at 10V
+#include <SpeedyStepper.h> //Simple & good stepper library, get it.
 
-#include <arduino.h>
-#include <GOTStateMachine.h>
-#include <wave.h>
+#include <TMCStepper.h>
 
-GOTStateMachine stateMachine(50); // execute every 50 milliseconds
-void StatePurge();
-void StateWave();
-void StateWait();
+#define DIR_PIN          3 // Direction
+#define STEP_PIN         4 // Step
+#define SERIAL_PORT Serial2 // HardwareSerial port pins 16 & 17
+#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
 
-// Configuration
-int lightPin1 = 5;
-int lightPin2 = 6;
-int motorPin1 = 7;
-int motorPin2 = 8;
-int motorPwmPin = 9;
-int lightPwmPin = 10;
+#define R_SENSE 0.11f // Match to your driver
+                      // SilentStepStick series use 0.11
+                      // UltiMachine Einsy and Archim2 boards use 0.2
+                      // Panucatt BSD2660 uses 0.1
+                      // Watterott TMC5160 uses 0.075
 
-void Forward()
-{
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);
-}
+bool shaft = false;  // ONLY NEEDED FOR CHANGING DIRECTION VIA UART, NO NEED FOR DIR PIN FOR THIS
 
-void Reverse()
-{
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, HIGH);
-}
+
+TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+
+SpeedyStepper stepper;
 
 void setup() {
-  Serial.begin(9600);
+
+stepper.connectToPins(STEP_PIN, DIR_PIN); // INITIALIZE SpeedyStepper
   
-  // Turn on light
-  pinMode(lightPin1, OUTPUT);
-  pinMode(lightPin2, OUTPUT);
-  pinMode(lightPwmPin, OUTPUT);
-  digitalWrite(lightPin1, HIGH);
-  digitalWrite(lightPin2, LOW);
-  analogWrite(lightPwmPin, 255);
-  // Initialize motor
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  pinMode(motorPwmPin, OUTPUT);
-  analogWrite(motorPwmPin, 0);
-  Forward();
+SERIAL_PORT.begin(115200);      // INITIALIZE UART TMC2209
+Serial.begin(9600);
+delay(500);
+Serial.println(F("Serial Initialized"));
 
-  // stateMachine.setStartState(StatePurge);
-  stateMachine.setStartState(StateWave);
+  driver.begin();                // Initialize driver
+                           
+  driver.toff(5);                 // Enables driver in software
+
+  driver.rms_current(600);       // Set motor RMS current
+  driver.microsteps(2);            // Set microsteps to 1/2
+
+  driver.pwm_autoscale(true);    // Needed for stealthChop
+  driver.en_spreadCycle(true);   // false = StealthChop / true = SpreadCycle
+
+  stepper.setCurrentPositionInSteps(0);                   // Set zero position
+  stepper.setSpeedInStepsPerSecond(400);              //Set Speed
+  stepper.setAccelerationInStepsPerSecondPerSecond(400);   //Set acceleration, smaller value for super smooth direction changing
+
 }
 
-void loop() 
-{
-  stateMachine.execute();
-}
+void loop() {
 
-void StatePurge()
-{
-    analogWrite(motorPwmPin, 255);
-
-    if (stateMachine.isDelayComplete(5000))
-      stateMachine.changeState(StateWait);
-}
-
-void StateWave()
-{
-    float durationMs = 2000;
-    float amplitudeTwentheMotor = 60;
-    float amplitudePSUMotor = 255;
-
-    int amplitude = amplitudePSUMotor;
-    float progress = (millis() - stateMachine.getCurrentStateStartTime()) / durationMs;
-    float value = sin(progress * PI); // [0, pi] -> [-1,1]
-    value = value * 0.5 + 0.5; // [0,1]
-    value *= amplitude;
-    Serial.println(value);
-    analogWrite(motorPwmPin, value); //Controlling speed (0 = off and 255 = max speed)
-
-    if (stateMachine.isDelayComplete(durationMs))
-      stateMachine.changeState(StateWait);
-}
-
-void StateWait()
-{
-  if(stateMachine.isFirstTime())
-    analogWrite(motorPwmPin, 0);
+uint16_t msread=driver.microsteps();
+Serial.print(F("Read microsteps via UART to test UART receive : "));    Serial.println(msread); 
   
-  if (stateMachine.isDelayComplete(10000))
-    stateMachine.changeState(StateWave);
+Serial.println(F("Move 6400 steps forward at 600ma"));
+driver.rms_current(600); 
+stepper.moveToPositionInSteps(6400);
+
+Serial.println(F("Wait 3sec and turn current low so you can turn the motor shaft"));
+driver.rms_current(10); 
+delay(3000);
+
+Serial.println(F("Move back to 0 position at 300ma"));
+driver.rms_current(300); 
+stepper.moveToPositionInSteps(0);
+
+//MOVE MOTOR VIA UART AND CHANGE DIRECTION VIA SOFTWARE, IT RUNS AS LONG AS YOU LET IT... PROBABLY ONLY USEFUL WITH ENCODER. THE VALUE SETS ONLY THE SPEED.
+
+driver.VACTUAL(16000); //SET SPEED OF MOTOR
+delay(2000); // MOTOR MOVES 2 SEC THEN STOPS
+driver.VACTUAL(0); //STOP MOTOR BY SETTING SPEED TO 0
+shaft = !shaft; // REVERSE DIRECTION
+driver.shaft(shaft); // SET DIRECTION
+
 }
